c55b689977bc89852619957bf8e5c8f74111e56b
Harm Aarts <harmaarts@gmail.com>
2011-06-29 17:08:08 +0200

    no longer a plugin, no longer init.rb is loaded which contained a method definition

--- lib/translate.rb
+++ lib/translate.rb
@@ -2,7 +2,7 @@ module Translate
   def self.locales_dir=(dir)
     @locales_dir = dir.to_s
   end
-   
+
   def self.locales_dir
     @locales_dir || Rails.root.join("config", "locales").to_s
   end
@@ -14,4 +14,11 @@ Dir[File.join(File.dirname(__FILE__), "translate", "*.rb")].each do |file|
   require file
 end
 
-  
+# TODO: Use new method available_locales once Rails is upgraded, see:
+# http://github.com/svenfuchs/i18n/commit/411f8fe7c8f3f89e9b6b921fa62ed66cb92f3af4
+# kill me......
+def I18n.valid_locales
+  I18n.backend.send(:init_translations) unless I18n.backend.initialized?
+  backend.send(:translations).keys.reject { |locale| locale == :root }
+end
+
